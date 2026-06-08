95229560177772357619792909dd725836abd344
L3-3O <leonatan2500@gmail.com>
2026-06-08 11:30:10 -0300

    AULA 05

---
+++ UC1/AULA_05/assets/js/script.js
@@ -0,0 +1,22 @@
+// VARIÁVEL
+let nome = "Jardel 20"; 
+nome = "Aluno" + " " + nome;
+nome = "Estudante" + " " +  nome;
+
+console.log ("Nome:" + nome)
+
+const nomeDev = "Jardel"
+
+
+console.log("Uso constante: " +nomeDev)
+
+let nome1 = "Jardel"
+let idade = "19"
+let estadocivil = "solteiro"
+let totaldefilhos = 0
+let naturalidade = "Fortaleza"
+let estado = "Ceará"
+
+let dados = "\n Nome2: " + nome + "\n Idade: " + idade +  "\n Estado Civil: " + estadocivil + "\n Naturalidade: " + naturalidade + "\n Estado: " + estado; 
+
+console.log(dados)
