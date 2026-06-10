d9e257050d2f52aa24e0b5b6711bba1cd7ccd28b
L3-3O <leonatan2500@gmail.com>
2026-06-10 11:31:51 -0300

    AULA_07

---
+++ UC1/AULA_07/assets/js/script.js
@@ -0,0 +1,35 @@
+// análise booleana
+let status = true
+let acao = prompt("Indique uma ação 0 ou 1")
+
+if(acao!=="" && acao<=1 && acao>=0) {
+    
+    let confirmar =confirm("Deseja alterar o sistema?");
+    
+    if(confirmar){
+        
+        if(acao===0){
+            status=false
+            console.log(`O SISTEMA ESTÁ DESATIVADO
+                O STATUS É ${status}`)
+        } else {(`O SISTEMA ESTÁ ATIVADO
+            O STATUS É ${status}`)
+        }
+
+        console.log(`Você confirmou a alteração ${acao}
+            `)
+
+    } else { 
+        alert("Você cancelou a alteração")
+
+
+
+    }
+    
+
+
+    console.log(`Você informou ${acao}`)
+
+} else {
+    alert("Você não informou o código ")
+}
